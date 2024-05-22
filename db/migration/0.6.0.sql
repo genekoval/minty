a446da01443e98cbf22a9bea25a1565412f28e74
Gene Koval <genyaremchuk@protonmail.com>
2024-05-22 02:05:57 -0400

    Add 'admin' role

--- db/migration/0.6.0.sql
+++ db/migration/0.6.0.sql
@@ -24,7 +24,8 @@ CREATE TABLE entity_link (
 CREATE TABLE user_account (
     user_id         uuid PRIMARY KEY REFERENCES entity_profile ON DELETE CASCADE,
     email           text UNIQUE NOT NULL,
-    password        text NOT NULL
+    password        text NOT NULL,
+    admin           boolean NOT NULL DEFAULT false
 );
 
 CREATE TABLE user_session (
@@ -39,12 +40,13 @@ WITH profile AS (
     SELECT profile_id, 'minty', true
     FROM profile
 )
-INSERT INTO user_account (user_id, email, password)
+INSERT INTO user_account (user_id, email, password, admin)
 SELECT
     profile_id,
     'minty@example.com',
     -- password is 'password'
-    '$argon2id$v=19$m=19456,t=2,p=1$l5wX+Rot3RRzyN4JFJMHKQ$mTZbLtNo3zyfJinzsgqgYdSuj/N1jz6i1AWLCGOna2o'
+    '$argon2id$v=19$m=19456,t=2,p=1$l5wX+Rot3RRzyN4JFJMHKQ$mTZbLtNo3zyfJinzsgqgYdSuj/N1jz6i1AWLCGOna2o',
+    true
 FROM profile;
 
 INSERT INTO entity_profile (profile_id, description, avatar, banner, created)
